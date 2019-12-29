cf27aa2287acc16e752fc20c36bbc1799a30629b
Chua Hui Shun <huishun@Chuas-MacBook-Pro.local>
2019-12-29 11:15:44 -0800

    style changes, added home button to fixed header

--- wp-content/themes/huishun-theme/header.php
+++ wp-content/themes/huishun-theme/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -7,52 +8,56 @@
     <!-- <title>Document</title> -->
     <?php wp_head(); ?>
 </head>
+
 <body>
     <header>
         <div class="fixed-header">
             <div class="fixed-header-inner">
-                    <div class="fixed-header-inner-container">
-                        <a href="<?php echo site_url() ?>" class="logo">
-                            <span class="hs">HS</span>
-                            <span class="logo-description">Code Factory</span>
-                        </a>
-                    </div>
+                <div class="fixed-header-inner-container">
+                    <a href="<?php echo site_url() ?>" class="logo">
+                        <span class="hs">HS</span>
+                        <span class="logo-description">Code Factory</span>
+                    </a>
+                </div>
 
-                    <div class="fixed-header-inner-container">
-                        
-                        <?php
-                        $args = array(
-                            'post_type' => 'page',
-                        );
-                        $pages = new WP_Query($args);
-                        $pages->posts = array_reverse($pages->posts);
-                        ?>
-
-                        <?php while ($pages->have_posts()) { $pages->the_post(); ?>
-                            <div class="fixed-header-item-wrapper">
-                                <a href="<?php echo the_permalink() ?>" class="header-label"><?php the_title() ?></a>
-                            </div>
-                        <?php } ?>
-    
-                        <?php wp_reset_query(); ?>
+                <div class="fixed-header-inner-container">
+                    <div class="fixed-header-item-wrapper">
+                        <a href="<?php echo site_url() ?>" class="header-label">Home</a>
                     </div>
+                    <?php
+                    $args = array(
+                        'post_type' => 'page',
+                    );
+                    $pages = new WP_Query($args);
+                    $pages->posts = array_reverse($pages->posts);
+                    ?>
+
+                    <?php while ($pages->have_posts()) {
+                        $pages->the_post(); ?>
+                        <div class="fixed-header-item-wrapper">
+                            <a href="<?php echo the_permalink() ?>" class="header-label"><?php the_title() ?></a>
+                        </div>
+                    <?php } ?>
+
+                    <?php wp_reset_query(); ?>
+                </div>
             </div>
         </div>
 
         <!-- <div class="archive-dropdown">
             <div class="">
 				<span class="archive-sidebar-title">Categories</span> -->
-				<?php 
-					// $categories = get_categories();
-					// foreach($categories as $category) {
-					//    	echo '<div class="sidebar-item"><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></div>' ;
-					// }
-				?>
-				<!-- </div>
+        <?php
+        // $categories = get_categories();
+        // foreach($categories as $category) {
+        //    	echo '<div class="sidebar-item"><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></div>' ;
+        // }
+        ?>
+        <!-- </div>
         </div> -->
 
         <div class="mobile-navi d-flex d-lg-none">
-        <div class="mobile-navi-container">
+            <div class="mobile-navi-container">
                 <div class="mobile-navi-item">
                     <a href="<?php echo site_url() ?>" class="logo">
                         <span class="hs">HS</span>
@@ -61,16 +66,16 @@
                 </div>
                 <div class="mobile-navi-item">
                     <a class="d-inline-block d-lg-none mobile-navi-trigger" target="#mobile-navi-main" href="javascript:void(0)" onclick="return false">
-                            <div class="hamburger">
-                                <span class="hamburger-line line-one"></span>
-                                <span class="hamburger-line line-two"></span>
-                                <span class="hamburger-line line-three"></span>
-                            </div>
+                        <div class="hamburger">
+                            <span class="hamburger-line line-one"></span>
+                            <span class="hamburger-line line-two"></span>
+                            <span class="hamburger-line line-three"></span>
+                        </div>
                     </a>
                 </div>
+            </div>
         </div>
-        </div>    
-        
+
     </header>
 
 
@@ -90,7 +95,8 @@
             $pages->posts = array_reverse($pages->posts);
             ?>
 
-            <?php while ($pages->have_posts()) { $pages->the_post(); ?>
+            <?php while ($pages->have_posts()) {
+                $pages->the_post(); ?>
                 <div class="mobile-navi-item-wrapper">
                     <a href="<?php echo the_permalink() ?>" class="header-label"><?php the_title() ?></a>
                 </div>
--- wp-content/themes/huishun-theme/index.php
+++ wp-content/themes/huishun-theme/index.php
@@ -1,12 +1,12 @@
-<?php get_header() ?>  
-    <div class="banner-wrapper">
-        <div class="section-container">
-            <!-- <div class="logo bigger">
+<?php get_header() ?>
+<div class="banner-wrapper">
+    <div class="section-container">
+        <!-- <div class="logo bigger">
                 <span class="hs">HS</span>
                 <span class="logo-description">Code Factory</span>
             </div> -->
-            <!-- <span class="banner-description"><?php bloginfo('description') ?></span> -->
-            <!-- <div class="scroll-down-container-outer">
+        <!-- <span class="banner-description"><?php bloginfo('description') ?></span> -->
+        <!-- <div class="scroll-down-container-outer">
                 <div class="scroll-down-container">
                     <a class="link scroll-down" href="javascript:void(0)">View Posts 
                         <svg
@@ -18,53 +18,56 @@
                     </a>
                 </div>
             </div>     -->
-        </div>
     </div>
+</div>
 
-    <div class="timeline-section">
-        <div class="section-container">
-                <h2 class="section-header">Newest posts</h2>
-                <div class="newest-posts-inner">
-                    <?php
-                    $default_posts_per_page = get_option( 'posts_per_page' );
-                    $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-                    $args = array( 'post_type' => 'post', 'posts_per_page' =>  3, 'paged' => $paged );
-                    $wp_query = new WP_Query($args);
-                    while(have_posts()) {
-                        the_post();
-                        get_template_part('content', 'archive');
-                    } ?>
-                </div>
-                <!-- then the pagination links -->
-                <div class="pagination">
-                    <span class="pagination-item"><?php next_posts_link( '&larr; Older posts', $wp_query->max_num_pages); ?></span>
-					<span class="pagination-item"><?php previous_posts_link( 'Newer posts &rarr;' ); ?></span>
-                </div>
+<div class="timeline-section">
+    <div class="section-container">
+        <h2 class="section-header">Newest posts</h2>
+        <div class="newest-posts-inner">
+            <?php
+            $default_posts_per_page = get_option('posts_per_page');
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+            $args = array('post_type' => 'post', 'posts_per_page' =>  3, 'paged' => $paged);
+            $wp_query = new WP_Query($args);
+            while (have_posts()) {
+                the_post();
+                get_template_part('content', 'archive');
+            } ?>
         </div>
+        <div class="scroll-down-container-outer">
+            <a class="link" href="<? get_page_link(get_page_by_title('All posts')) ?>">More Posts</a>
+        </div>
+        <!-- then the pagination links -->
+        <!-- <div class="pagination">
+            <span class="pagination-item"><?php next_posts_link('&larr; Older posts', $wp_query->max_num_pages); ?></span>
+            <span class="pagination-item"><?php previous_posts_link('Newer posts &rarr;'); ?></span>
+        </div> -->
     </div>
+</div>
 
-    <div class="cards-container">
+<div class="cards-container">
     <div class="row">
         <div class="col-md-4">
-        <a href="<?php echo get_category_link(get_cat_ID('getting started')) ?>" class="card getting-started-clicked">
-            <div class="card-head pastel-peach">
-                <div class="card-head-content">
-                    Get started
-                </div>    
-            </div>
-            <div class="d-none d-md-block card-body">
-              <p class="card-text">
-                  New to coding? Click here for the information you need to get started.
-              </p>
-            </div>
-        </a>
+            <a href="<?php echo get_category_link(get_cat_ID('getting started')) ?>" class="card getting-started-clicked">
+                <div class="card-head pastel-peach">
+                    <div class="card-head-content">
+                        Get started
+                    </div>
+                </div>
+                <div class="d-none d-md-block card-body">
+                    <p class="card-text">
+                        New to coding? Click here for the information you need to get started.
+                    </p>
+                </div>
+            </a>
         </div>
         <div class="col-md-4">
             <a href="<?php echo get_category_link(get_cat_ID('building things')) ?>" class="card">
                 <div class="card-head sky-blue">
                     <div class="card-head-content">
                         Build things
-                    </div>    
+                    </div>
                 </div>
                 <div class="d-none d-md-block card-body">
                     <p class="card-text">
@@ -78,32 +81,31 @@
                 <div class="card-head">
                     <div class="card-head-content pastel-pink">
                         HS Coding Logs
-                    </div>    
+                    </div>
                 </div>
                 <div class="d-none d-md-block card-body">
-                <p class="card-text">
-                    Learning in progress... find out how I learn coding! 
-                </p>
+                    <p class="card-text">
+                        Learning in progress... find out how I learn coding!
+                    </p>
                 </div>
             </a>
         </div>
     </div>
+</div>
+
+<div class="subscribe">
+    <div class="subscribe-outer">
+        <h2 class="section-header">JOIN ME ON THIS JOURNEY!</h2>
+        <form id="subscribe-form">
+
+            <input class="default-input" type="text" placeholder="Name" value="" name="FNAME">
+            <input class="default-input" type="email" placeholder="Email" value="" name="EMAIL">
+            <input class="default-button" type="submit" value="Subscribe" name="subscribe">
+
+        </form>
+
+        <div id="subscribe-result" class="subscribe-result"></div>
     </div>
-
-    <div class="subscribe">
-        <div class="subscribe-outer">
-                <h2 class="section-header">JOIN ME ON THIS JOURNEY!</h2>
-                <form
-                    id="subscribe-form">
-                                        
-                        <input class="default-input" type="text" placeholder="Name" value="" name="FNAME">
-                        <input class="default-input" type="email" placeholder="Email" value="" name="EMAIL">
-                        <input class="default-button" type="submit" value="Subscribe" name="subscribe">
-
-                </form>
-
-                <div id="subscribe-result" class="subscribe-result"></div> 
-        </div>
-    </div> 
+</div>
 
 <?php get_footer() ?>
